d015ce58991d77bab5289beba4cca1af889f84b5
tappashaikmohammadsamiullah <tappasamiullah7036@gmail.com>
2026-07-23 22:55:42 +0530

    Update app.py

--- my-cicd-project/my-cicd-project/app/app.py
+++ my-cicd-project/my-cicd-project/app/app.py
@@ -1,1 +1,10 @@
-print('Hello, CI/CD Project!')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "CI/CD Pipeline is Working Successfully!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
